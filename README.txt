1a8f9876b7ba85f832cd6483a0dc90cef694a4e2
Cuneyt Tas <cueneyt.tas@stud.uni-duisburg.essen.de>
2022-08-08 23:18:36 +0200

    Finalized Bot

---
+++ README.txt
@@ -0,0 +1,3 @@
+Before running the bot you have to create a file named "used.txt" in the same folder as the "scrape.py" file.
+Then you have to create a file named "apidata.txt" in the same folder and have to put your API Data for Tweepy(Consumer_Key, Consumer_Key_Secret, Access_Token, Access_Token_Secret,Bot@) seperated by Commas with no space.
+Then you can run the "scrape.py" script and enjoy your bot!
--- scrape.py
+++ scrape.py
@@ -3,14 +3,24 @@ from itertools import count
 from multiprocessing.connection import wait
 from threading import Thread
 from time import sleep
+import traceback
 from pyppeteer import launch
 import tweepy
 import keyboard
 
+def delete(api):
+
+    for status in tweepy.Cursor(api.user_timeline).items():
+             try:
+               api.destroy_status(status.id)
+               print("Deleted:", status.id)
+             except Exception:
+                print("Failed to delete:", status.id)
+    exit()
 
 async def main(country_code,city_code):
     # launch chromium browser in the background
-    browser = await launch(headless=False, args=['--disable-infobars', f'--window-size={1920},{1080}'])
+    browser = await launch(headless=True, args=['--disable-infobars', f'--window-size={1920},{1080}'])
     # open a new tab in the browser
     page = await browser.newPage()
 
@@ -24,20 +34,30 @@ async def main(country_code,city_code):
    
     
     await page.click("#select2-country-list-container")
-    await page.waitFor(200)
+    await page.waitFor(300)
     await page.keyboard.type(country_code)
-    
+    await page.waitFor(1000)
     await page.keyboard.press("Enter")
+
+    await page.waitFor(1000)
 
     await page.click("#select2-city-list-container")
-    await page.waitFor(200)
+    await page.waitFor(300)
     await page.keyboard.type(city_code)
-    
-    await page.keyboard.press("Enter")
-
-    
-    
     await page.waitFor(1000)
+    await page.keyboard.press("Enter")
+    await page.waitFor(1000)
+    
+
+    if(country_code.lower() == "türkiye"):
+        await page.click("#select2-district-list-container")
+        await page.waitFor(300)
+        await page.keyboard.type("a")
+        await page.waitFor(1000)
+        await page.keyboard.press("Enter")
+        await page.waitFor(1000)
+    
+    
     # create a screenshot of the page and save it
     screen = await page.xpath("/html/body/div[2]/div/section[2]/div/div")
     await screen[0].screenshot({"path": "python.png"})
@@ -47,10 +67,15 @@ async def main(country_code,city_code):
 
 
 
+with open("apidata.txt", "r") as f:
+    data = f.read()
+    splitted = data.split(",")
+
 
 auth = tweepy.OAuth1UserHandler(
-   "H6Lj8ynPiwzyTPX3QoNMN8zYV", "sR37yoPDkPga4tYNsoZ7NdAHLHnhmehE9FguljNzubHGNwVsQ5", "1555137243253194752-FvlD5enmUBXwCWjLQSekkL0pMqRrvv", "PoTbwiIzOEPujavJdk0bQzfqNXDMJUAX8K7Icax3er6lM"
+   splitted[0], splitted[1], splitted[2], splitted[3]
 )
+
 
 with open("used.txt", "r") as f:
     data = f.read()
@@ -66,22 +91,15 @@ loop = asyncio.get_event_loop()
 
 api = tweepy.API(auth)
 
-#for status in tweepy.Cursor(api.user_timeline).items():
-            #try:
-                #api.destroy_status(status.id)
-                #print("Deleted:", status.id)
-            #except Exception:
-                #traceback.print_exc()
-                #print("Failed to delete:", status.id)
 
-
+#delete(api)
 
 i = 0
 j=0
 while i == 0:
     if keyboard.is_pressed("q"):
         break
-    tweets = api.search_tweets(q = "@BotFazilet", result_type ="recent", count=10,tweet_mode="extended")
+    tweets = api.search_tweets(q = "@"+splitted[4], result_type ="recent", count=50,tweet_mode="extended")
     for j in range(0,len(tweets)):
      if(tweets[j] != []):
       tweetid = tweets[j].id
@@ -93,7 +111,12 @@ while i == 0:
         sleep(5)
      else:
         try:
+         
+         
          temp = tweets[j].full_text.split("#")
+         temp[1] = temp[1].replace(" ","")
+         temp[2] = temp[2].replace(" ","")
+        
          loop.run_until_complete(main(temp[1],temp[2]))
          api.update_status_with_media('your reply',"python.png", in_reply_to_status_id = tweetid , auto_populate_reply_metadata=True)
          data = data+ str(tweetid)
@@ -109,7 +132,14 @@ while i == 0:
          sleep(5) 
          print("replied")
         except Exception:
+            traceback.print_exc
             print("ERROR IN INPUT")
+
+
+
+
+
+
 
     
 
